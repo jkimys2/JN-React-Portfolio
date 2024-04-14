7b01b4641b6a1ea28b6a5a68b5f2e9bff9dc9468
Jordan Newhouse <jkimys2@gmail.com>
2024-04-13 23:23:16 -0400

    contact page update

---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  <div>
+    <h1>You can contact me via:</h1>
+    <a href="https://github.com/jkimys2">GitHub</a>
+    <a href="mailto:jkimys2@gmail.com">Email</a>
+  </div>;
+}
