568fb008985cb1a4caea8b8c59a819dc16cd6874
Jordan Newhouse <jkimys2@gmail.com>
2024-04-13 22:29:35 -0400

    main file update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,45 @@
+// Import dependencies
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {createBroweserRouter, RouterProvider} from "react-router-dom"
+import { createBroweserRouter, RouterProvider } from "react-router-dom";
+
+// Import files/components
+import App from "./App.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Portfolio from "./components/Portfolio.jsx";
+import Resume from "./components/Resume.jsx";
+
+// Define accessible routes and which components respond to which URL
+const router = createBroweserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
