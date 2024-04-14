0484d525705ef500d5cf7c9f563f179d43232bf0
Jordan Newhouse <jkimys2@gmail.com>
2024-04-14 17:42:41 -0400

    contact page update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
 
   return (
     <div className="container text-center">
-      <h1>Hello {name}</h1>
+      <h1>Contact Me!</h1>
       <form className="form" onSubmit={handleFormSubmit}>
         <input
           value={email}
