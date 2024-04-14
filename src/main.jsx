6cc2d35c963ba0090356953bd6decbc825e5adec
Jordan Newhouse <jkimys2@gmail.com>
2024-04-13 22:30:50 -0400

    error page added

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { createBroweserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
+import Error from "./components/Error.jsx";
 import Portfolio from "./components/Portfolio.jsx";
 import Resume from "./components/Resume.jsx";
 
