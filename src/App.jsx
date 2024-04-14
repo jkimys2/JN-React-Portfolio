d6e4b0018174e87762865be3a2d7cf0fa26499a8
Jordan Newhouse <jkimys2@gmail.com>
2024-04-13 22:19:05 -0400

    app bootstrap update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import About from "./components/About";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
