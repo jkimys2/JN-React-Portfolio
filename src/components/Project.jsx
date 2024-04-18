fd994cbd2b2cddb952070243d9e54c854e9aabda
Jordan Newhouse <jkimys2@gmail.com>
2024-04-18 19:23:17 -0400

    project page update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,12 @@
+// Import dependencies
+import { Link } from "react-router-dom";
+
 export default function Project({ title, link, github, image }) {
   return (
     <div>
       <h3>{title}</h3>
+      <Link to={link}>Deployed Page</Link>
+      <Link to={github}>GitHub</Link>
       <a href={link}>Deployment</a> <br />
       <a href={github}>GitHub</a> <br />
       <img src={image} />
